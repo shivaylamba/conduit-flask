970703b0eb6964911de454f25340b7e44df6dbdb
shivaylamba <shivaylamba@gmail.com>
2024-02-27 13:24:36 +0530

    initiate pydantic validation

--- realworld/routes/v1/users.py
+++ realworld/routes/v1/users.py
@@ -3,6 +3,7 @@ from extensions import couchbase_db
 from models.users import User
 from utils.jwt import generate_token
 from utils.auth import authenticate
+from validation.auth import RegisterRequest, RegisterResponse, LoginRequest, ErrorResponse
 import bcrypt
 import uuid
 
@@ -16,7 +17,6 @@ users_blueprint = Blueprint(
 def apply_authentication_middleware():
     # List of routes that require authentication
     auth_required_routes = ["users_endpoints.get_current_user"]
-    print("-----Request Endpoint-------", request.endpoint)
 
     # Check if the current request endpoint is in the list of routes that require authentication
     if request.endpoint in auth_required_routes:
@@ -28,52 +28,71 @@ def apply_authentication_middleware():
 
 @users_blueprint.route("/", methods=["POST"])
 def create_user() :
-    print("------Creating user------")
-    data = request.json
-    # TODO: Request Validation
-
-    # Check if the username already exists
-    existing_user = User.get_by_username(data["username"])
-    existing_user_email = User.get_by_email(data["email"])
+    data = RegisterRequest(**request.json)
+    existing_user = User.get_by_username(data.username)
+    existing_user_email = User.get_by_email(data.email)
 
     if existing_user:
-        return jsonify({"error": "Username already exists"}), 400
+        return jsonify(ErrorResponse(**{"error": "Username already exists"})), 400
     elif existing_user_email:
-        return jsonify({"error": "Email already exists"}), 400
+        return jsonify(ErrorResponse(**{"error": "Email already exists"})), 400
 
 
     user_id = str(uuid.uuid4())
-    user = User(user_id, data["username"], data["email"], data["password"])
+    user = User(user_id, data.username, data.email, data.password)
     user.save()
 
     token = generate_token(user_id)
-    return jsonify({"user": {"username": user.username, "email": user.email, "token": token}}), 201
+    register_response = RegisterResponse( **{
+        "user": {
+            "username": user.username, 
+            "email": user.email, 
+            "token": token
+            }
+        }
+    )
+    # Return the JSON response using jsonify
+    return jsonify(register_response.model_dump()), 201
 
-@users_blueprint.route("/login", methods=["POST"])
+@users_blueprint.route("/login", methods=["POST"], )
 def login_user() :
-    data = request.json
-    # TODO: Request Validation
-
-    user = User.get_by_email(data["email"])
+    data = LoginRequest(**request.json)
+    user = User.get_by_email(data.email)
     if not user:
-        return jsonify({"error": "email or password is incorrect"}), 400
+        return jsonify(ErrorResponse(**{"error": "email or password is incorrect"})), 400
 
-    if bcrypt.checkpw(data["password"].encode('utf-8'), user.password.encode('utf-8')):
+    if bcrypt.checkpw(data.password.encode('utf-8'), user.password.encode('utf-8')):
         # Passwords match, generate JWT token and return it
         token = generate_token(user.user_id)
-        return jsonify({"user": {"username": user.username, "email": user.email, "token": token}}), 200
+        login_response = RegisterResponse( **{
+            "user": {
+                "username": user.username, 
+                "email": user.email, 
+                "token": token
+            }
+        }
+    )
+        return jsonify(login_response.model_dump()), 200
     else:
         # Passwords don't match
-        return jsonify({"error": "Invalid password"}), 401
+        return jsonify(ErrorResponse(**{"error": "Invalid password"})), 401
 
 
 @users_blueprint.route("/get-current-user", methods=["GET"])
 def get_current_user():
     user = g.current_user
     if user:
-        return jsonify({"user": {"user_id" : user.user_id, "username": user.username, "email": user.email}}), 200
+        return jsonify(
+            RegisterResponse( **{
+                "user": {
+                    "user_id" : user.user_id, 
+                    "username": user.username, 
+                    "email": user.email
+                }
+            })
+        ), 200
     else:
-        return jsonify({"error": "Not logged in"}), 401
+        return jsonify(ErrorResponse(**{"error": "Not logged in"})), 401
 
 def get_all_users() :
     return {
---
+++ realworld/validation/auth.py
@@ -0,0 +1,51 @@
+from pydantic import BaseModel, model_validator, ValidationError
+
+class RegisterRequest(BaseModel):
+    username: str
+    password: str
+    email: str
+
+    @model_validator(mode='before')
+    def validate_all(cls, fields):
+        for key in fields.keys():
+            if not fields[key]:
+                raise ValidationError(f"Validation failed for {key}")
+            
+        return fields
+
+class NestedRegisterResponse(BaseModel):
+    username: str
+    email: str
+    token: str
+
+class RegisterResponse(BaseModel):
+    user: NestedRegisterResponse
+
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str
+
+    @model_validator(mode='before')
+    def validate_all(cls, fields):
+        for key in fields.keys():
+            if not fields[key]:
+                raise ValidationError(f"Validation failed for {key}")
+            
+        return fields
+    
+
+class CurrentUserRequest(BaseModel):
+    email: str
+    password: str
+
+    @model_validator(mode='before')
+    def validate_all(cls, fields):
+        for key in fields.keys():
+            if not fields[key]:
+                raise ValidationError(f"Validation failed for {key}")
+            
+        return fields
+    
+class ErrorResponse:
+    error: str
